b437574e39bac6205b412f3c29cdc132c2bae9f2
Fedor Mukhin <71270225+TheTedLab@users.noreply.github.com>
2023-04-09 19:54:18 +0300

    Update predict.py

--- cats-vs-dogs/predict.py
+++ cats-vs-dogs/predict.py
@@ -1,10 +1,16 @@
 import os
+import random
+import numpy as np
+import matplotlib.pyplot as plt
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from keras import models
+from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 
 base_dir = r'/mnt/d/cats_vs_dogs_small'
 test_dir = os.path.join(base_dir, 'test')
+
+sample_dir = r'/mnt/d/Cats_vs_Dogs/data/test'
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -30,3 +36,21 @@ print(f'Pretrained VGG16 model accuracy: {test_acc}\nPretrained VGG16 model loss
 model_fine_tunning = models.load_model('models/cats_and_dogs_small_fine_tunning.h5')
 test_loss, test_acc = model_fine_tunning.evaluate(test_generator, steps=50)
 print(f'Fine-tunned VGG16 model accuracy: {test_acc}\nFine-tunned VGG16 model loss: {test_loss}')
+
+sample_str = str(random.randint(1, 12500)) + '.jpg'
+img_path = os.path.join(sample_dir, sample_str)
+img = image.image_utils.load_img(img_path, target_size=(150, 150))
+
+plt.imshow(img)
+
+# Тестовое изображение
+x = image.image_utils.img_to_array(img)
+
+# Переформируем его в (1, 150, 150, 3)
+x = x.reshape((1,) + x.shape)
+
+predict_value = model_fine_tunning.predict(x)
+print(predict_value[0][0])
+print('cat' if predict_value == 0.0 else 'dog')
+
+plt.show()
