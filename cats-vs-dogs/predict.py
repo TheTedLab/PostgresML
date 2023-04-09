2db16314029a74252d383ec2a7ad6f4a663647f2
Fedor Mukhin <71270225+TheTedLab@users.noreply.github.com>
2023-04-09 18:19:21 +0300

    Update predict and add fine tunning model
    Add fine-tunning model and graphs

--- cats-vs-dogs/predict.py
+++ cats-vs-dogs/predict.py
@@ -1,4 +1,5 @@
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from keras import models
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -15,17 +16,17 @@ test_generator = test_datagen.flow_from_directory(
 
 
 model_standard = models.load_model('models/cats_and_dogs_small_standard.h5')
-test_loss, test_acc = model_standard.evaluate_generator(test_generator, steps=50)
-print(f'Standard model accuracy: {test_acc}\n Standard model loss: {test_loss}')
+test_loss, test_acc = model_standard.evaluate(test_generator, steps=50)
+print(f'Standard model accuracy: {test_acc}\nStandard model loss: {test_loss}')
 
 model_data_aug = models.load_model('models/cats_and_dogs_small_data_aug.h5')
-test_loss, test_acc = model_data_aug.evaluate_generator(test_generator, steps=50)
-print(f'Data augmented model accuracy: {test_acc}\n Data augmented model loss: {test_loss}')
+test_loss, test_acc = model_data_aug.evaluate(test_generator, steps=50)
+print(f'Data augmented model accuracy: {test_acc}\nData augmented model loss: {test_loss}')
 
 model_pretrained = models.load_model('models/cats_and_dogs_small_pretrained.h5')
-test_loss, test_acc = model_data_aug.evaluate_generator(test_generator, steps=50)
-print(f'Pretrained VGG16 model accuracy: {test_acc}\n Pretrained VGG16 model loss: {test_loss}')
+test_loss, test_acc = model_data_aug.evaluate(test_generator, steps=50)
+print(f'Pretrained VGG16 model accuracy: {test_acc}\nPretrained VGG16 model loss: {test_loss}')
 
 model_fine_tunning = models.load_model('models/cats_and_dogs_small_fine_tunning.h5')
-test_loss, test_acc = model_data_aug.evaluate_generator(test_generator, steps=50)
-print(f'Fine-tunned VGG16 model accuracy: {test_acc}\n Fine-tunned VGG16 model loss: {test_loss}')
+test_loss, test_acc = model_data_aug.evaluate(test_generator, steps=50)
+print(f'Fine-tunned VGG16 model accuracy: {test_acc}\nFine-tunned VGG16 model loss: {test_loss}')
